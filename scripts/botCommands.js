bdd1030203c26439df6d3a008399924097a93bd4
m00se333 <moosecunningham@gmail.com>
2016-05-17 21:12:48 -0700

    added another response

--- scripts/botCommands.js
+++ scripts/botCommands.js
@@ -1,5 +1,16 @@
 module.exports = function(robot) {
-  robot.hear(/These boots/, function(res){
-    return res.send("...are made for walkin'")
+  robot.hear(/Who is your pet?/, function(res){
+    return res.send("A'lar")
   });
+}
+
+module.exports = function(robot) {
+  robot.respond(/Hi Hubot! My name is (.*)/i, function(message){
+    var name = message.match[1];
+    if (name.toLowerCase() == "kael'bot" || name == "kael'bot sunstrider") {
+      return message.send("You are not Kael'bot Sunstrider- I am Kael'bot Sunstrider!";)
+    } else {
+      return message.reply("Nice to meet you, " + name + "!");
+    }
+  } )
 }
