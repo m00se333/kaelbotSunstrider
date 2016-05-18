deae4041d49dff9a1bda1e7495cf934ea7e7d664
m00se333 <moosecunningham@gmail.com>
2016-05-17 21:39:11 -0700

    inital kaelbot commit

--- scripts/botCommands.js
+++ scripts/botCommands.js
@@ -1,16 +1,18 @@
+//hear is the bot just listening to something, not dependent on an @, if the text is typed it sends it.
 module.exports = function(robot) {
   robot.hear(/Who is your pet?/, function(res){
     return res.send("A'lar")
   });
 }
 
+//respond is a call to the bot so @Kaelbot, it responds.
 module.exports = function(robot) {
   robot.respond(/Hi Hubot! My name is (.*)/i, function(message){
-    var name = message.match[1];
+    var name = message.match[1]; // The first group is the entire expression, use index value [1]
     if (name.toLowerCase() == "kaelbot" || name == "kaelbot sunstrider") {
       return message.send("You are not Kael'bot Sunstrider- I am Kael'bot Sunstrider!";)
     } else {
       return message.reply("Nice to meet you, " + name + "!");
     }
-  } )
+  })
 }
