cf7548dcbe817e2b2ca0a060e8d901c802e7a20d
m00se333 <moosecunningham@gmail.com>
2016-05-19 14:24:13 -0700

    changed instances of kael'bot... into kaelbot for simplicity's sake and added a few test commands in botCommands.js

--- scripts/botCommands.js
+++ scripts/botCommands.js
@@ -1,12 +1,8 @@
 //hear is the bot just listening to something, not dependent on an @, if the text is typed it sends it.
-module.exports = function(robot) {
-  robot.hear(/Who is your pet?/, function(res){
-    return res.send("A'lar")
-  });
-}
 
 //respond is a call to the bot so @Kaelbot, it responds.
-module.exports = function(robot) {
+/* module.exports = function(robot) {
+  
   robot.respond(/Hi Hubot! My name is (.*)/i, function(message){
     var name = message.match[1]; // The first group is the entire expression, use index value [1]
     if (name.toLowerCase() == "kaelbot" || name == "kaelbot sunstrider") {
@@ -14,5 +10,52 @@ module.exports = function(robot) {
     } else {
       return message.reply("Nice to meet you, " + name + "!");
     }
-  })
+  });
+
+  robot.hear(/Who is your pet?/, function(res){
+    return res.send("A'lar")
+  });
 }
+*/
+
+module.exports = function(robot) {
+  
+  // Magic Missle 
+  robot.respond(/target (.*)/i, function(message){
+    var mMissles = ["pyroblast", "ice lance", "poison nova", "disenegrate"];
+    var name = message.match[1];
+    var selectedSpell = mMissles[Math.floor(Math.random()*mMissles.length)];
+
+    if (selectedSpell === mMissles[0]) {
+      return message.send(robot.name + " winds up a " + selectedSpell.toUpperCase() + " and hurls it at " + name + "!");
+    } else if (selectedSpell === mMissles[1]) {
+      return message.send(robot.name + " spams " + selectedSpell.toUpperCase() + " at " + name + "!");
+    } else if (selectedSpell === mMissles[2]) {
+      return message.send(robot.name + " emits a " + selectedSpell.toUpperCase() + " and " + name + " was standing right in the middle of it!")
+    } else {
+      return message.send(robot.name + " plants his feet in the ground and casts " + selectedSpell.toUpperCase() + " right at " + name + " and guess what it's already off cooldown...")
+    }
+
+  });
+
+  //General "hear" code
+  robot.hear(/who is the most powerful user of magic?/i, function(res){
+    return res.send("I am of course.")
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
