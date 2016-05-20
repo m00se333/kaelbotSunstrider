e7c19ea50102278b6fddd691fe4e0248319dca69
m00se333 <moosecunningham@gmail.com>
2016-05-19 19:53:26 -0700

    add changes in class to fix merge conflicts

--- scripts/botCommands.js
+++ scripts/botCommands.js
@@ -45,7 +45,7 @@ module.exports = function(robot) {
 
   //random 
   robot.respond(/practice/i, function(res){
-    var spells = ["*felo'mena'SHA!*", "*felo'mena'SHAW!*", "_felo... mena... SHA!_", "*malore!*", "_felomenashaaa!_b"];
+    var spells = ["*felo'mena'SHA!*", "*felo'mena'SHAW!*", "_felo... mena... SHA!_", "*malore!*", "_felomenashaaa!_"];
     return res.send(res.random(spells));
   });
 }
