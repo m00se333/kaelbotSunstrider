2608556aa0979e69f2ca0783e9ddf4f622acbbf4
m00se333 <moosecunningham@gmail.com>
2016-05-19 21:22:40 -0700

    debug reply discrepancy

--- scripts/botCommands.js
+++ scripts/botCommands.js
@@ -29,43 +29,44 @@ module.exports = function(robot) {
     var name = message.match[1];
     var selectedSpell = mMissles[Math.floor(Math.random()*mMissles.length)];
 
-    if (selectedSpell === mMissles[0]) {
-      return message.reply(robot.name + " winds up a " + selectedSpell.toUpperCase() + " and hurls it at " + name + "!");
-    } else if (selectedSpell === mMissles[1]) {
-      return message.reply(robot.name + " spams " + selectedSpell.toUpperCase() + " at " + name + "!");
-    } else if (selectedSpell === mMissles[2]) {
-      return message.reply(robot.name + " emits a " + selectedSpell.toUpperCase() + " and " + name + " was standing right in the middle of it!")
-    } else {
-      return message.reply(robot.name + " plants his feet in the ground and casts " + selectedSpell.toUpperCase() + " right at " + name + " and guess what it's already off cooldown...")
-    }
+    message.reply("hello" + name);
+    // if (selectedSpell === mMissles[0]) {
+    //   return message.reply(robot.name + " winds up a " + selectedSpell.toUpperCase() + " and hurls it at " + name + "!");
+    // } else if (selectedSpell === mMissles[1]) {
+    //   return message.reply(robot.name + " spams " + selectedSpell.toUpperCase() + " at " + name + "!");
+    // } else if (selectedSpell === mMissles[2]) {
+    //   return message.reply(robot.name + " emits a " + selectedSpell.toUpperCase() + " and " + name + " was standing right in the middle of it!")
+    // } else {
+    //   return message.reply(robot.name + " plants his feet in the ground and casts " + selectedSpell.toUpperCase() + " right at " + name + " and guess what it's already off cooldown...")
+    // }
 
   });
 
-  //General "hear" code
-  robot.hear(/who is the most powerful user of magic/i, function(res){
-    return res.send("I am of course.")
-  });
+  // //General "hear" code
+  // robot.hear(/who is the most powerful user of magic/i, function(res){
+  //   return res.send("I am of course.")
+  // });
 
-  //random 
-  robot.respond(/practice/i, function(res){
-    var spells = ["*felo'mena'SHA!*", "*felo'mena'SHAW!*", "_felo... mena... SHA!_", "*malore!*", "_felomenashaaa!_"];
-    return res.send(res.random(spells));
+  // //random 
+  // robot.respond(/practice/i, function(res){
+  //   var spells = ["*felo'mena'SHA!*", "*felo'mena'SHAW!*", "_felo... mena... SHA!_", "*malore!*", "_felomenashaaa!_"];
+  //   return res.send(res.random(spells));
 
-  });
+  // });
 
-  robot.respond(/what can you tell us about magic and trees?/i, function(res){
-    var magictrees = ["Hazel", "Cedar", "Willow", "Redwood"];
-    var htmlstring = "I know about ";
-      for (var i = 0; i<magictrees.length; i++) {
-        htmlstring += magictrees[i] + "trees, ";
+  // robot.respond(/what can you tell us about magic and trees?/i, function(res){
+  //   var magictrees = ["Hazel", "Cedar", "Willow", "Redwood"];
+  //   var htmlstring = "I know about ";
+  //     for (var i = 0; i<magictrees.length; i++) {
+  //       htmlstring += magictrees[i] + "trees, ";
 
-        if (magictrees[i] === 3) {
-          htmlstring += " and " + magictrees[3] + "trees."
-        }
+  //       if (magictrees[i] === 3) {
+  //         htmlstring += " and " + magictrees[3] + "trees."
+  //       }
 
-      }
-    console.log(htmlstring);
-  })
+  //     }
+  //   console.log(htmlstring);
+  // })
 
 }
 
